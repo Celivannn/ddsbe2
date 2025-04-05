0dc093a71b863a5d791b5a0677c9b0bff2949e53
Celivannn <sinadjanivan@gmail.com>
2025-04-05 14:12:46 +0800

    Newest Code

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,17 @@
 <?php
 
-/** @var \Laravel\Lumen\Routing\Router $router */
-
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
+$router->get('/users', ['uses' => 'UserController@getUsers']);
 
-    $router->get('/users',['uses' => 'UserController@getUsers']);
+    $router->get('/users', 'UserController@index');//get all users records
+    $router->post('/users', 'UserController@addUser');//create new user record
+    $router->get('/users/{id}', 'UserController@show');//get user by id
+    $router->put('/users/{id}', 'UserController@update');//update user record
+    $router->patch('/users/{id}', 'UserController@update');//update user record
+    $router->delete('/users/{id}', 'UserController@delete');//delete record
 
-// Grouping API routes
-    $router->get('/users',['uses' => 'UserController@getUsers']);
-    $router->get('/users', 'UserController@getUsers'); // Get all users
-    $router->get('/users/{id}', 'UserController@show'); // Get user by ID
-    $router->post('/users', 'UserController@add'); // Create new user
-    $router->put('/users/{id}', 'UserController@update'); // Update user
-    $router->patch('/users/{id}', 'UserController@update'); // Partial update
-    $router->delete('/users/{id}', 'UserController@delete'); // Delete user
-
-    $router->get('/usersjob', 'UserJobController@index'); // Get all user jobs
-    $router->get('/userjob/{id}', 'UserJobController@show'); // Get user job by ID
+    $router->get('/usersjob', 'UserJobController@index'); // Get all jobs
+    $router->get('/usersjob/{id}', 'UserJobController@show'); // Get job by ID
